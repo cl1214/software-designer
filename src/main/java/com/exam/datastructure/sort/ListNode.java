4c404576800551fefd8209fc998c1afd98202387
chenlin <1563452201@qq.com>
2023-10-26 17:49:40 +0800

    fix

--- src/main/java/com/exam/datastructure/sort/ListNode.java
+++ src/main/java/com/exam/datastructure/sort/ListNode.java
@@ -2,6 +2,10 @@ package com.exam.datastructure.sort;
 
 import lombok.Data;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 @Data
 public class ListNode {
     int val;
@@ -47,17 +51,115 @@ public class ListNode {
         return (int) result;
     }
 
+    public static int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        Map<String, Integer> strMap = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        strMap.put("IV", 4);
+        strMap.put("IX", 9);
+        strMap.put("XL", 40);
+        strMap.put("XC", 90);
+        strMap.put("CD", 400);
+        strMap.put("CM", 900);
+        if (Objects.isNull(s)) {
+            return 0;
+        }
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char cur = s.charAt(i);
+            if (i <= s.length() - 2) {
+                String str = s.substring(i, i + 2);
+                if ("IV".equals(str)) {
+                    result += 4;
+                    i++;
+                    continue;
+                } else if ("IX".equals(str)) {
+                    result += 9;
+                    i++;
+                    continue;
+                } else if ("XL".equals(str)) {
+                    result += 40;
+                    i++;
+                    continue;
+                } else if ("XC".equals(str)) {
+                    result += 90;
+                    i++;
+                    continue;
+                } else if ("CD".equals(str)) {
+                    result += 400;
+                    i++;
+                    continue;
+                } else if ("CM".equals(str)) {
+                    result += 900;
+                    i++;
+                    continue;
+                }
+            }
+            if ('I' == cur) {
+                result += 1;
+            } else if ('V' == cur) {
+                result += 5;
+            } else if ('X' == cur) {
+                result += 10;
+            } else if ('L' == cur) {
+                result += 50;
+            } else if ('C' == cur) {
+                result += 100;
+            } else if ('D' == cur) {
+                result += 500;
+            } else if ('M' == cur) {
+                result += 1000;
+            }
+        }
+        return result;
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        String result = "";
+        Character[] arr = new Character[200];
+        int length = strs[0].length();
+        for (int i = 1; i < strs.length; i++) {
+            if (strs[i].length() < length) {
+                length = strs[i].length();
+            }
+        }
+        if (length == 0) {
+            return result;
+        }
+        for (int i = 0; i < strs.length; i++) {
+            String str = strs[i];
+            for (int j = 0; j < length; j++) {
+                Character s = arr[j];
+                char c = str.charAt(j);
+                if (s == null) {
+                    arr[j] = c;
+                }
+                if (i > 0 && (s != c)) {
+                    length = j;
+                    break;
+                }
+            }
+        }
+        for (int k = 0; k < length; k++) {
+            if (Objects.isNull(arr[k])) {
+                break;
+            }
+            result += arr[k];
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
-        ListNode listNode5 = new ListNode(5);
-        ListNode listNode4 = new ListNode(4, listNode5);
-        ListNode listNode3 = new ListNode(3, listNode4);
-        ListNode listNode2 = new ListNode(2, listNode3);
-        ListNode listNode1 = new ListNode(1, listNode2);
-        //ListNode listNode = reverseList(listNode1);
-        ListNode listNode = reverseList(listNode1);
-        reverse(-123);
-        isPalindrome(-123);
-        System.out.println(listNode1);
+        System.out.println(longestCommonPrefix(new String[]{"ab","a", ""}));
+        System.out.println(longestCommonPrefix(new String[]{"flower","flow","flight"}));
+        System.out.println(longestCommonPrefix(new String[]{"flower","flower","flower"}));
+        System.out.println(longestCommonPrefix(new String[]{"dog","racecar","car"}));
 
     }
 }
